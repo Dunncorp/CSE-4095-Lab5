2943264d957f7cfdf65bf1ab959d7261b336c4c9
wonder-phil <phillip.g.bradford@gmail.com>
2021-10-18 07:10:08 -0400

    update

--- lb.js
+++ lb.js
@@ -8,6 +8,8 @@ const handler = (req, res) => {
   // Pipe the vanilla node HTTP request (a readable stream) into `request`
   // to the next server URL. Then, since `res` implements the writable stream
   // interface, you can just `pipe()` into `res`.
+  
+ 
   req.pipe(request({ url: servers[cur] + req.url })).pipe(res);
   cur = (cur + 1) % servers.length;
 };
